2d46dc5baffc5bf048f32b4e78d350ddd281228d
TA <pepper.ppeatns@gmail.com>
2019-06-08 18:52:21 +0900

    Updates

---
+++ precache-manifest.1674527c2c810440e62dbe7e57d81f29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4c2fa8e2aba4bf2fbf0f5a0d2fee94",
+    "url": "/Abnb-clone/index.html"
+  },
+  {
+    "revision": "803bcc7acecb9363957b",
+    "url": "/Abnb-clone/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "9b97f40551be152c50cc",
+    "url": "/Abnb-clone/static/css/main.1f04067c.chunk.css"
+  },
+  {
+    "revision": "803bcc7acecb9363957b",
+    "url": "/Abnb-clone/static/js/2.d70cc0f9.chunk.js"
+  },
+  {
+    "revision": "9b97f40551be152c50cc",
+    "url": "/Abnb-clone/static/js/main.db064d86.chunk.js"
+  },
+  {
+    "revision": "96553508eb5206de1dca",
+    "url": "/Abnb-clone/static/js/runtime~main.70b4f906.js"
+  },
+  {
+    "revision": "c9c6bec8c8edd0526945eaab2cd13a02",
+    "url": "/Abnb-clone/static/media/laConcha.c9c6bec8.jpg"
+  },
+  {
+    "revision": "909c9fbe8ff913ef14b84faf50d58b7a",
+    "url": "/Abnb-clone/static/media/plzCons-img.909c9fbe.jpg"
+  }
+]);
